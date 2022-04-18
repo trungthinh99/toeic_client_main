e34879b6f5f51aeb068e24ad8b774e3eb9d595bb
trungthinh99 <trungthinh.99@gmail.com>
2022-04-19 00:27:59 +0700

    content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { Layout } from 'antd';
 import AppHeader from './components/header/AppHeader';
+import Home from './components/homepage/Home';
 
 function App() {
-  return (
-    <div className="App">
-        <AppHeader />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout>
+                <AppHeader />
+                <Home />
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
